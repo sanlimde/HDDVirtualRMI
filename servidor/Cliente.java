8cbe2d14c6d95585f1b120c7b6ef91c2f18bd2e3
joscabmar <jacm.legend@gmail.com>
2015-04-23 20:01:56 +0200

    Eliminado el ID del cliente porque es innecesario. El mismo cliente puede acceder desde varias máquinas y no debe ser necesario recordar el ID. Dejo la clase por si se piensa ampliar las funcionalidades de cada cliente (incluir contraseña u otro tipo de datos)

--- servidor/Cliente.java
+++ servidor/Cliente.java
@@ -10,19 +10,13 @@ public class Cliente implements Serializable {
      */
     private String nombre;
 
-    /**
-     * Identificador del cliente
-     */
-    private String iD;
 
     /**
      * Constructor principal
      * @param nom Nombre del cliente
-     * @param id Identificador del cliente
      */
     Cliente(String nom, String id){
         nombre = nom;
-        iD = id;
     }
 
     /**
@@ -33,19 +27,12 @@ public class Cliente implements Serializable {
         return nombre;
     }
 
-    /**
-     * Método que devuelve el identificador del cliente
-     * @return identificador del cliente
-     */
-    public String getID(){
-        return iD;
-    }
 
     /**
      * Método que devuelve una cadena con el nombre e identificador del cliente
-     * @return nombre e identificador del cliente
+     * @return nombre del cliente
      */
     public String toString(){
-        return nombre + " | " + iD;
+        return nombre;
     }
 }
